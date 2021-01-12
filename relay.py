0da5d24dd3aa3848f7c83572d5924d7e251e086b
stickandstone <putonstack@gmail.com>
2021-01-13 00:25:33 +0800

    add bot and req.txt

--- relay.py
+++ relay.py
@@ -6,10 +6,23 @@ GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 print("Start")
 RELAIS_1_GPIO = 17 #pin 11
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-print("GPIO LOW")
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-time.sleep(5)
-print("GPIO HIGH")
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-time.sleep(5)
+
+for i in range(3):
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+	GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+	time.sleep(1)
+	GPIO.cleanup()
+	time.sleep(1)
+
+# GPIO.output(RELAIS_1_GPIO, 1) # out
+# time.sleep(2)
+# print("GPIO LOW")
+# GPIO.output(RELAIS_1_GPIO, 0) # out
+# time.sleep(2)
+# GPIO.cleanup()
+
+# print("GPIO HIGH")
+# GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+# time.sleep(5)
 print("Exit")
