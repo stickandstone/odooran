55e1433953682c0aa97f6595a1fcdd71189a943e
stickandstone <35613446+stickandstone@users.noreply.github.com>
2021-01-16 13:22:15 +0800

    button implementation

--- odo_bot.py
+++ odo_bot.py
@@ -6,15 +6,22 @@
 import logging
 from uuid import uuid4
 
-from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, Update
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext
-from telegram.utils.helpers import escape_markdown
+# from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, Update, KeyboardButton
+# from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext
+# from telegram.utils.helpers import escape_markdown
+
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+
+
 import sys
-try:
-    if sys.argv[1] == 'test':
-        import relay_fake
-except:
+
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    pass
+else:
     import relay
+
 
 SECRET = open('secret.txt').read()
 
@@ -34,6 +41,19 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('''Привет! Это одоран, бот который открывает гаражные ворота. 
     Используй /click для открытия, остановки или закрытия двери.''')
 
+    test_text = 'Open the gate!'
+
+    keyboard = [
+        [
+            InlineKeyboardButton(test_text, callback_data='1')
+        ],
+        [InlineKeyboardButton("Option 3", callback_data='3')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
 
 def make_click(update: Update, context: CallbackContext) -> None:
     global gate_is_close
@@ -48,13 +68,23 @@ def make_click(update: Update, context: CallbackContext) -> None:
     relay.click()
 
 
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text=f"Selected option: {query.data}")
+
+
 def main() -> None:
     updater = Updater(SECRET, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("click", make_click))
-
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
     # Start the Bot
     updater.start_polling()
 
